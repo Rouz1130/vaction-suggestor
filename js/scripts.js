e6e76d016a4aa91083e0ba20730820b19cc29e95
rouz majlessi <rmajlessi@gmail.com>
2016-06-24 15:34:24 -0700

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-$(document).ready(function){
-  
-}
+$(document).ready(function(){
+  $("#blanks").submit(function(event){
+    event.preventDefault();
+
+    var time = $("#time").val();
+    var sightC = $("#sightC").val();
+    var relaxing = $("#relaxing").val();
+    var concert = $("#concert").val();
+    var weather = $("#weather").val();
+
+    if (time === "yes") {
+      $("#events").show();
+    }
+
+
+    if (sightC === "yes") {
+      $("#sightC").show();
+    }
+
+
+    if (relaxation === "yes") {
+      $("#relaxing").show();
+    }
+
+
+    if(events === "yes") {
+      $("#concert").show();
+    }
+
+
+    if(climate === "yes") {
+      $("#weather").show();
+    }
+
+  });
+});
